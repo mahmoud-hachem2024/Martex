6bee75fb298fa4294cfa1a0ddd4085ab6412ad12
mahmoud <146541832+mahmoud-hachem2024@users.noreply.github.com>
2024-03-24 14:36:08 +0100

    add bootstarp et fontawsome

---
+++ martex/src/components/navBar/NavBar.js
@@ -0,0 +1,27 @@
+import { Link, Outlet }  from "react-router-dom";
+
+
+
+const NavBar =()=>{
+    return (
+        <>
+        <nav>
+            <ul>
+                <li>
+                    <Link to="/">Home</Link>
+                </li>
+                <li>
+                    <Link to="/about">About</Link>
+                </li>
+                <li>
+                    <Link to="/login">Login</Link>
+                </li>
+                
+            </ul>
+
+        </nav>
+        <Outlet/>
+        </>
+    )
+};
+export default NavBar;
